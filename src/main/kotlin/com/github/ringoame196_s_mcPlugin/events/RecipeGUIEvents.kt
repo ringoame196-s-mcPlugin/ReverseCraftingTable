449d3d2c58f123f2cdc2aaa622889c3f63c1d466
りんご飴196 <kazumac41@gmail.com>
2025-01-26 20:02:57 +0900

    ⬆️  :GUI関係を一つのクラスに

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/RecipeGUIEvents.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/events/RecipeGUIEvents.kt
@@ -7,12 +7,29 @@ import org.bukkit.Sound
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.InventoryView
-import org.bukkit.inventory.ItemStack
 
-class InventoryClickEvent : Listener {
+class RecipeGUIEvents : Listener {
     private val recipeGUIManager = RecipeGUIManager()
+    private val inSlot = recipeGUIManager.inItemSlot
+
+    @EventHandler
+    fun onPlayerInteract(e: PlayerInteractEvent) {
+        val player = e.player
+        val block = e.clickedBlock ?: return
+        val sneak = player.isSneaking
+
+        if (!sneak) return
+        if (e.action != Action.RIGHT_CLICK_BLOCK) return
+        if (block.type != Material.CRAFTING_TABLE) return
+        val gui = recipeGUIManager.makeGUI()
+        e.isCancelled = true
+        player.openInventory(gui)
+    }
 
     @EventHandler
     fun onInventoryClick(e: InventoryClickEvent) {
@@ -38,22 +55,34 @@ class InventoryClickEvent : Listener {
     private fun recipeCraft(gui: InventoryView, player: Player) {
         val inItemSlot = recipeGUIManager.inItemSlot
         val selectItem = gui.getItem(inItemSlot)
-        val selectItemType = selectItem?.type
-        val selectItemAmount = selectItem?.amount ?: 0
-        val recipeItemList = RecipeManager.acquisitionRecipeItem(selectItemType ?: Material.AIR)
+        val recipeItemData = RecipeManager.acquisitionRecipeItem(selectItem ?: return)
+        val recipeItemList = recipeItemData?.recipeItemList
+        val multiple = recipeItemData?.multiple ?: 0
+        val inItem = recipeItemData?.inItem
 
-        if (recipeItemList.isEmpty()) {
+        if (recipeItemList == null || recipeItemList.isEmpty()) {
             val sound = Sound.BLOCK_NOTE_BLOCK_BELL
             player.playSound(player, sound, 1f, 1f)
         } else {
             for (item in recipeItemList) {
-                val amount = item.amount * selectItemAmount
+                val amount = item.amount * multiple
                 item.amount = amount
                 player.inventory.addItem(item)
             }
             val sound = Sound.BLOCK_ANVIL_USE
             player.playSound(player, sound, 1f, 1f)
-            gui.setItem(inItemSlot, ItemStack(Material.AIR))
+            gui.setItem(inItemSlot, inItem)
         }
+    }
+
+    @EventHandler
+    fun onInventoryClose(e: InventoryCloseEvent) {
+        val player = e.player
+        val gui = e.view
+        val guiTitle = gui.title
+
+        if (guiTitle != recipeGUIManager.guiTitle) return
+        val item = gui.getItem(inSlot) ?: return
+        player.inventory.addItem(item)
     }
 }
