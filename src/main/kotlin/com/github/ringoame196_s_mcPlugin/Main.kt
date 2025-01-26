2ebec8e075dd85c08d8a88b6213d53f1c23bc4b6
りんご飴196 <kazumac41@gmail.com>
2025-01-26 20:03:19 +0900

    ⬆️  :1つにまとめる

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Main.kt
@@ -1,14 +1,12 @@
 package com.github.ringoame196_s_mcPlugin
 
-import com.github.ringoame196_s_mcPlugin.events.InventoryClickEvent
-import com.github.ringoame196_s_mcPlugin.events.PlayerInteractEvent
+import com.github.ringoame196_s_mcPlugin.events.RecipeGUIEvents
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     private val plugin = this
     override fun onEnable() {
         super.onEnable()
-        server.pluginManager.registerEvents(PlayerInteractEvent(), plugin)
-        server.pluginManager.registerEvents(InventoryClickEvent(), plugin)
+        server.pluginManager.registerEvents(RecipeGUIEvents(), plugin)
     }
 }
